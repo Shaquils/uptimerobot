af015beff83eb5237d37e9852d9a39f73157bbf4
Shaquils <70125972+Shaquils@users.noreply.github.com>
2020-08-29 09:10:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://adroit-.glitch.me", "https://dreamcloud-.glitch.me"]
+const urls = ["https://dreamcloud-.glitch.me"] 
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
